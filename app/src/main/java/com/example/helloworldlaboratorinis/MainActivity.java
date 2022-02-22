8c9ff5a67aac104a9c517885e1c17bd7a7561f18
Benciukas <benas.bulauka@gmail.com>
2022-02-22 21:52:44 +0200

    Antras sucommitinimas

--- app/src/main/java/com/example/helloworldlaboratorinis/MainActivity.java
+++ app/src/main/java/com/example/helloworldlaboratorinis/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.helloworldlaboratorinis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void onBtnMainClick(View view) {
+        TextView txtMain=(TextView)findViewById(R.id.txtMain);
+        txtMain.setText("Naujas tekstas");
     }
 }
