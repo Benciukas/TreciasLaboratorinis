6d1176ca0489bb0b2112e2cb1b4ebd1588a4a7e5
Benciukas <benas.bulauka@gmail.com>
2022-03-13 11:27:45 +0200

    ProjectBlankTemplateV3

--- 2labV2/app/src/main/java/com/example/a2labv2/MainActivity.java
+++ 2labV2/app/src/main/java/com/example/a2labv2/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.a2labv2;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @Nullable
+    @Override
+    public ActionBar getSupportActionBar() {
+        return super.getSupportActionBar();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
