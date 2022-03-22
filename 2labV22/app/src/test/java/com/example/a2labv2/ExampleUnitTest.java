719dce1d4bdec1d191e98ae79d2bec93d602fd2c
Benciukas <100218783+Benciukas@users.noreply.github.com>
2022-03-22 18:59:51 +0200

    Update ExampleUnitTest.java

--- 2labV22/app/src/test/java/com/example/a2labv2/ExampleUnitTest.java
+++ 2labV22/app/src/test/java/com/example/a2labv2/ExampleUnitTest.java
@@ -11,7 +11,45 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void givenResult_EmptyText_CalcElementCount() {
+        String givenText = "";
+        int expectedValue = 0;
+        int actualValue = calcWordorChar.getCharsCount(givenText);
+        assertEquals(expectedValue,actualValue);
+    }
+    @Test
+    public void givenResult_EmptyText_CalcWordCount() {
+        String givenText = "";
+        int expectedValue = 1;
+        int actualValue = calcWordorChar2.getCharsCount2(givenText);
+        assertEquals(expectedValue,actualValue);
+    }
+    @Test
+    public void givenResult_BasicText_CalcElementCount() {
+        String givenText = "NormalTextTest";
+        int expectedValue = 14;
+        int actualValue = calcWordorChar.getCharsCount(givenText);
+        assertEquals(expectedValue,actualValue);
+    }
+    @Test
+    public void givenResult_BasicText_CalcWordCount() {
+        String givenText = "NormalTextTest";
+        int expectedValue = 1;
+        int actualValue = calcWordorChar2.getCharsCount2(givenText);
+        assertEquals(expectedValue,actualValue);
+    }
+    @Test
+    public void givenResult_SpaceText_CalcElementCount() {
+        String givenText = "Normal Text With Space";
+        int expectedValue = 22;
+        int actualValue = calcWordorChar.getCharsCount(givenText);
+        assertEquals(expectedValue,actualValue);
+    }
+    @Test
+    public void givenResult_SpaceText_CalcWordCount() {
+        String givenText = "Normal Text With Space";
+        int expectedValue = 4;
+        int actualValue = calcWordorChar2.getCharsCount2(givenText);
+        assertEquals(expectedValue,actualValue);
     }
 }
